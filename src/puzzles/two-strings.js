36f88b2cce15f4d8e921b5eeb8cf6815cad0cabc
Bruno Pinto <brosapinto@gmail.com>
2018-09-07 12:23:04 +0100

    Adding Typescript support

--- src/puzzles/two-strings.js
+++ src/puzzles/two-strings.js
@@ -1,4 +1,19 @@
 /**
+ * Very naive algorithm that keeps only unique values of a collection (aka Set)
+ * @param {tring|Array|Array-like} iterable Iterable collection
+ * @returns {Array} List of unique items in the collection
+ */
+const unique = iterable => {
+  let obj = {};
+
+  for (let item of iterable) {
+    obj[item] = {};
+  }
+
+  return Object.keys(obj);
+};
+
+/**
  * Check whether s1 and s2 share a common char
  * https://www.hackerrank.com/challenges/two-strings/problem
  * @param {String} s1
@@ -10,7 +25,8 @@ export default function twoStrings(s1, s2) {
     return true;
   }
 
-  const uniqChars = new Set(s1.split(""));
+  //const uniqChars = new Set(s1.split(""));
+  const uniqChars = unique(s1);
 
   for (let char of uniqChars) {
     if (s2.indexOf(char) !== -1) {
